86a21a24fe0656578fa213db1a5ba457a4f62e23
thanhphuocit <thanhphuoc3687118@gmail.com>
2024-10-28 23:24:37 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import React,{ useContext } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 
-function App() {
+const App =() => {
+  const {audioRef, track} = useContext(PlayerContext);
   return (
     <div className="h-screen bg-black">
       <div className='h-[90%] flex'>
@@ -12,8 +14,8 @@ function App() {
            <Display/>
       </div>
       <Player/> 
+      <audio ref ={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
-
 export default App
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -18,12 +18,11 @@ const Display = () => {
     }
   })
 
-
   return (
     <div ref={displayRef} className='w-[100] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
         <Route path ='/' element ={<DisplayHome/>}/>
-        <Route path ='/Album/:id' element ={<DisplayAlbum/>}/>
+        <Route path ='/Album/:id' element ={<DisplayAlbum/>}/> 
       </Routes>
     </div>
   )
